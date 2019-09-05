b78e23f971cb5717b34d5db63cd66b0345eaa78f
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2019-09-05 16:11:49 +0200

    Make copy of whole 'gradle-groovy-dsl' example, at revision '7b885e16aa67b6f379e25df86844b809971a1da5', to be used as base of 'gradle-kotlin-dsl' example.

---
+++ gradle-kotlin-dsl/java-app/src/main/java/eu/xword/labs/gc/JavaAppApplication.java
@@ -0,0 +1,12 @@
+package eu.xword.labs.gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAppApplication.class, args);
+    }
+}
